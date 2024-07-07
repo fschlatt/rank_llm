b7e1f22b9b36e54d21f42f0ba15df5e14677d424
Maik Fr?be <kibi9872@webis3.medien.uni-weimar.de>
2024-07-07 08:48:03 +0200

    fix typo

--- re-rank-plaid-x/prepare-rerank-file-from-plaid-x.py
+++ re-rank-plaid-x/prepare-rerank-file-from-plaid-x.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import pandas as pd
 from tira.rest_api_client import Client
 from tira.third_party_integrations import ir_datasets
 tira = Client()
@@ -10,13 +11,14 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Load rerank file')
 
     parser.add_argument('--input-dataset', type=str, help='The directory with the input data (i.e., a queries.jsonl and a documents.jsonl file).', required=True)
+    parser.add_argument('--input-run', type=str, help='The input run to re-rank.', required=True)
     parser.add_argument('--top-k', type=int, help="how many documents to rerank", required=True)
     parser.add_argument('--output', type=str, help='The output will be stored in this directory.', required=True)
 
     return parser.parse_args()
 
-def rerank_df(input_dataset, top_k):
-    plaidX = tira.pd.from_retriever_submission('reneuir-2024/reneuir-baselines/plaid-x-retrieval', input_dataset)
+def rerank_df(input_dataset, input_run, top_k):
+    plaidX = pd.read_csv(input_run, sep="\s+", names=["qid", "q0", "docno", "rank", "score", "system"], dtype={"qid": str, "docno": str})
     plaidX = plaidX[plaidX['rank'] <= top_k]
     dataset = ir_datasets.load(input_dataset)
     queries = {i.query_id: i.default_text() for i in dataset.queries_iter()}
@@ -30,7 +32,7 @@ def rerank_df(input_dataset, top_k):
 
 if __name__ == '__main__':
     args = parse_args()
-    df = rerank_df(args.input_dataset, args.top_k)
+    df = rerank_df(args.input_dataset, args.input_run, args.top_k)
 
     with gzip.open(f'{args.output}/rerank.jsonl.gz', 'wt') as f:
         for _, i in df.iterrows():
--- re-rank-plaid-x/rank-llm-rerank-plaid-x.sh
+++ re-rank-plaid-x/rank-llm-rerank-plaid-x.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-tira-cli download --dataset $1 --approach reneuir-2024/reneuir-baselines/plaid-x-retrieval
+INPUT_RUN=$(tira-cli download --dataset $1 --approach reneuir-2024/reneuir-baselines/plaid-x-retrieval)
 
-/prepare-rerank-file-from-plaid-x.py --output /tmp/rerank.jsonl.gz --input-dataset $1
+/prepare-rerank-file-from-plaid-x.py --output /tmp/ --input-dataset $1 --input-run ${INPUT_RUN}/run.txt --top-k 100 
+
+zcat /tmp/rerank.jsonl.gz |head -10
 
 python3 /workspace/rank_llm/scripts/run_rank_llm.py \
 	--model_path=castorini/rank_zephyr_7b_v1_full \
 	--top_k_candidates=100 \
-	--dataset=tmp/rerank.jsonl.gz \
+	--dataset=/tmp/rerank.jsonl.gz \
 	--prompt_mode=rank_GPT \
 	--context_size=4096 \
 	--variable_passages \
